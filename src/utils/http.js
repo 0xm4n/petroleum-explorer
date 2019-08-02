e123bba8c00375ad6607f02bd4ed750c1a0f07a8
zhenyit <zhenyit@outlook.com>
2019-08-02 14:37:10 -0600

    Update http.js

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://127.0.0.1:3000/', // url = base url + request url
+  baseURL: 'http://35.238.247.40:3000/', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 50000 // request timeout
+  timeout: 30000 // request timeout
 })
 
 export default http
